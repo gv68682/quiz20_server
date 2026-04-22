8f6531da1bf043868455177ae16c6d7d1e24f144
GreenMind <chinni@Padmapriyas-MacBook-Air.local>
2026-04-21 23:34:28 -0400

    initial commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB Connection
 const MONGODB_URI = process.env.MONGODB_URI;
