f95d28dd9371b52f9bb29e3ea4cec9d6dd7a4a01
GreenMind <chinni@Padmapriyas-MacBook-Air.local>
2026-05-23 21:04:01 -0400

    remove node-fetch

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const axios = require('axios');
-const fetch = require('node-fetch');
 require('dotenv').config();
 const app = express();
 app.use(cors());
